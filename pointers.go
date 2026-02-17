8f6a39d83fccc1f0408d8fe9dbb0250d79256117
MateusVasc <mvaraujo2004@gmail.com>
2026-02-16 22:52:28 -0300

    feat: learning pointers and memory in go

---
+++ pointers.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name string
+}
+
+func changeName(u User) {
+	u.Name = "Mateus"
+}
+
+func changeName2(u *User) {
+	u.Name = "Mateus"
+}
+
+type User2 struct {
+	Scores []int
+}
+
+func main() {
+	fmt.Println("Hello")
+
+	u1 := User{Name: "Hiraeth"}
+	changeName(u1)
+	fmt.Println(u1)
+
+	changeName2(&u1)
+	fmt.Println(u1)
+
+	fmt.Println("Slices")
+	u2 := User2{Scores: []int{10, 20}}
+	u3 := u2
+	u3.Scores[0] = 999
+	fmt.Println(u2.Scores)
+}
+
+/*
+	Pointers em Go
+
+	FUNDAMENTO 1: TUDO EM GO É PASSADO POR VALOR
+
+	- Go sempre passa cópia
+	- Se é pra compartilhar, você passa um pointer
+
+	----------------- Exemplo em Java
+
+	User u1 = new User("Ana");
+	User u2 = u1;
+	u2.name = "Carlos";
+
+	(O nome de "u1" VAI SER SOBRESCRITO)
+
+	----------------- Exemplo em Go
+
+	type User struct {
+    	Name string
+	}
+
+	u1 := User{Name: "Ana"}
+	u2 := u1
+	u2.Name = "Carlos" ()
+
+	("u2" vira UMA CÓPIA. Nada é sobrescrito)
+
+	FUNDAMENTO 2: SLICES SÃO DIFERENTES...
+
+
+*/
